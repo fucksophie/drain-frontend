dc869e9aba5cf7910bd3da4cdd9d93c3a5a24ec2
friend <friend@yourfriend.lv>
2022-01-30 00:50:03 +0200

    V1

---
+++ main.js
@@ -0,0 +1,90 @@
+$( ".window" ).draggable({ handle: ".header" });
+
+const volumeBar = document.getElementById("volume");
+const audioElement = document.getElementById("audio");
+const controlElement = document.getElementById("control");
+
+audioElement.volume = volumeBar.value/100;
+
+if(localStorage.getItem("volume")) {
+	audioElement.volume = localStorage.getItem("volume")/100;
+	volumeBar.value = localStorage.getItem("volume");
+}
+
+volumeBar.addEventListener('click', function (e) {
+    var value_clicked = e.offsetX * this.max / this.offsetWidth;
+    this.value = value_clicked;
+	audioElement.volume = value_clicked/100;
+
+	localStorage.setItem("volume", value_clicked);
+});
+
+controlElement.addEventListener('click', () => {
+	if(audioElement.paused) {
+		audioElement.play();
+		controlElement.innerText = 'pause';
+	} else {
+		audioElement.pause()
+		controlElement.innerText = 'play';
+	}
+})
+
+const backend = location.hostname === "localhost" 
+			|| location.hostname === "127.0.0.1" ? 
+		"ws://localhost:3000" : 
+		"wss://drain-backend.yourfriend.lv";
+
+const ws = new WebSocket(`${backend}/ws`);
+const listenerElement = document.getElementById("listeners");
+const artistElement = document.getElementById("artist");
+const titleElement = document.getElementById("title");
+
+const progressElement = document.getElementById("progress");
+const releasedClassElement = document.querySelector(".released");
+const releasedElement = document.getElementById("released");
+
+let song;
+
+
+ws.addEventListener("message", async ev => {
+	if(ev.data.startsWith("//")) {
+		console.log('[SERVER] ' + ev.data)
+		return;
+	}
+
+	const data = JSON.parse(ev.data);
+
+	console.log(data);
+
+	if(data.type == "listeners") {
+		listenerElement.innerText = data.count;
+	}
+
+	if(data.type == "song") {
+		audioElement.src = backend.replace("ws", "http")+"/"+data.song.file;
+		audioElement.currentTime = data.playingFor/1000;
+		artistElement.innerText = data.song.artist;
+		titleElement.innerText = data.song.title;
+		if(song) audioElement.play(); // doesn't autoplay if we replace the src
+
+		song = data.song;
+		if(song.date) {
+			releasedClassElement.style.visibility = "visible";
+			
+			releasedElement.innerText = song.date;
+		} else {
+			releasedClassElement.style.visibility = "hidden";
+		}
+	}
+
+	if(data.type == "timeUpdate") {
+		const time = data.playingFor/1000;
+		const diff = time - audioElement.currentTime;
+		progressElement.value = data.playingFor/song.duration*100
+
+		if(diff > 1.2) {
+			console.log("Out of sync!!")
+			audioElement.currentTime = data.playingFor/1000;
+		}
+	}
+})

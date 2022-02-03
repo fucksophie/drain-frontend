859616703d9c11cf4810c4041e62f143fd5e41d1
friend <friend@yourfriend.lv>
2022-02-03 18:31:12 +0200

    tiny refactor

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 $( ".window" ).draggable({ handle: ".header" });
 
+const backend = location.hostname === "localhost" 
+			|| location.hostname === "127.0.0.1" ? 
+		"ws://localhost:3000" : 
+		"wss://drain-backend.yourfriend.lv";
+
+const ws = new WebSocket(`${backend}/ws`);
+
 const volumeBar = document.getElementById("volume");
 const audioElement = document.getElementById("audio");
 const controlElement = document.getElementById("control");
+const listenerElement = document.getElementById("listeners");
+const artistElement = document.getElementById("artist");
+const titleElement = document.getElementById("title");
+
+const progressElement = document.getElementById("progress");
+const releasedClassElement = document.querySelector(".released");
+const releasedElement = document.getElementById("released");
+const playlistElement = document.getElementById("playlist");
+
+let song;
+let playlist;
 
 audioElement.volume = volumeBar.value/100;
 
@@ -19,42 +37,21 @@ volumeBar.addEventListener('click', function (e) {
 	localStorage.setItem("volume", value_clicked);
 });
 
-controlElement.addEventListener('click', () => {
-	if(audioElement.paused) {
-		audioElement.play();
-		controlElement.innerText = 'pause';
-	} else {
-		audioElement.pause()
-		controlElement.innerText = 'play';
-	}
-})
-
-const backend = location.hostname === "localhost" 
-			|| location.hostname === "127.0.0.1" ? 
-		"ws://localhost:3000" : 
-		"wss://drain-backend.yourfriend.lv";
-
-const ws = new WebSocket(`${backend}/ws`);
-const listenerElement = document.getElementById("listeners");
-const artistElement = document.getElementById("artist");
-const titleElement = document.getElementById("title");
-
-const progressElement = document.getElementById("progress");
-const releasedClassElement = document.querySelector(".released");
-const releasedElement = document.getElementById("released");
-const playlistElement = document.getElementById("playlist");
-
-let song;
-let playlist;
-
 ws.addEventListener("message", async ev => {
 	const data = JSON.parse(ev.data);
 
-	if(data.type == "listeners") listenerElement.innerText = data.count;
+	if(data.type == "listeners") {
+		if(data.count == 1) {
+			listenerElement.innerText = "1 listener"
+		} else {
+			listenerElement.innerText = data.count + " listeners"
+		}
+	}
 
 	if(data.type == "song") {
-		audioElement.src = backend.replace("ws", "http")+"/"+data.song.file;
-		audioElement.currentTime = data.playingFor/1000;
+		audioElement.src = backend.replace("ws", "http") + "/" + data.song.file;
+
+		audioElement.currentTime = data.playingFor / 1000;
 		artistElement.innerText = data.song.artist;
 		titleElement.innerText = data.song.title;
 		
@@ -81,10 +78,7 @@ ws.addEventListener("message", async ev => {
 		const diff = time - audioElement.currentTime;
 		progressElement.value = data.playingFor/song.duration*100
 
-		if(diff > 1.2) {
-			console.log("Out of sync!!")
-			audioElement.currentTime = data.playingFor/1000;
-		}
+		if(diff > 1.2) audioElement.currentTime = data.playingFor/1000;
 	}
 
 	if(data.type == "playlist") {
@@ -92,31 +86,21 @@ ws.addEventListener("message", async ev => {
 	
 		playlist.forEach(song => {
 			const songElement = document.createElement("li");
+			
 			songElement.innerText = song.artist + " - " + song.title;
 			songElement.id = song.file;
+
 			playlistElement.appendChild(songElement);
 		})
 	}
+})
 
-	if(data.type == "window") {
-		const windowElement = document.createElement('div');
-		windowElement.style.width = data.width + "px";
-
-		windowElement.style.height = data.height + "px";
-		windowElement.className = "window";
-
-		const headerElement = document.createElement('div');
-		headerElement.className = "header";
-
-		windowElement.appendChild(headerElement);
-		const containerElement = document.createElement("container");
-
-		containerElement.innerHTML = data.container;
-		containerElement.className = "container";
-
-		windowElement.appendChild(containerElement);
-		document.body.appendChild(windowElement);
-
-		$( ".window" ).draggable({ handle: ".header" });
+controlElement.addEventListener('click', () => {
+	if(audioElement.paused) {
+		audioElement.play();
+		controlElement.innerText = 'pause';
+	} else {
+		audioElement.pause()
+		controlElement.innerText = 'play';
 	}
 })
